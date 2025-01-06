184cd33df8a3a2f725cb5851063bd51d5a73a721
Krishna Pensalwar <100767867+KrishnaPensalwar@users.noreply.github.com>
2025-01-06 15:48:33 +0530

    Time: 13 ms (46.22%), Space: 18.4 MB (7.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& a, int target) {
-        vector<int> ans;
-        int n=a.size();
+        // vector<int> ans;
+        // int n=a.size();
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(a[i] + a[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(a[i] + a[j] == target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //             return ans;
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
+        map<int, vector<int>> mp;
+
+        for (int i = 0; i < a.size(); i++) {
+            mp[a[i]].push_back(i);
+        }
+
+        for (int i = 0; i < a.size(); i++) {
+            int complement = target - a[i];
+            if (mp.find(complement) != mp.end()) {
+                for (int idx : mp[complement]) {
+                    if (idx != i) { 
+                        return {i, idx};
+                    }
                 }
             }
         }
-
-        return ans;
+        return {};
     }
 };
