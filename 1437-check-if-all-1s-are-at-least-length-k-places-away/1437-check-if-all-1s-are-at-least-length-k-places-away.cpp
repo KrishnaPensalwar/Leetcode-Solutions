b92ecc3e1b8ddf86ab86be43c8345e6b1d6b8772
Krishna Pensalwar <100767867+KrishnaPensalwar@users.noreply.github.com>
2025-11-18 22:27:57 +0530

    Time: 0 ms (100%), Space: 61.5 MB (48.06%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        vector<int> one_places;
+        // vector<int> one_places;
+        int lastone = -99999;
 
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
-                one_places.push_back(i);
+                if(i-lastone < k+1){
+                    return false;
+                }else{
+                    lastone = i;
+                }
             }
         }
 
-        if(one_places.size()<2){
-            return true;
-        }
+        // if(one_places.size()<2){
+        //     return true;
+        // }
 
-        for(int i=0;i<one_places.size()-1;i++){
-            if(one_places[i+1]-one_places[i]<k+1){
-                return false;
-            }
-        }
+        // for(int i=0;i<one_places.size()-1;i++){
+        //     if(one_places[i+1]-one_places[i]<k+1){
+        //         return false;
+        //     }
+        // }
 
         return true;
     }
