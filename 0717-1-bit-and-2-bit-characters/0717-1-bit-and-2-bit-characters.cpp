944db7023baacc6f92a1cae1e068d260d4cba60b
Krishna Pensalwar <100767867+KrishnaPensalwar@users.noreply.github.com>
2025-11-18 22:17:20 +0530

    Time: 0 ms (100%), Space: 13.1 MB (41.51%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -3,14 +3,18 @@ public:
     bool isOneBitCharacter(vector<int>& bits) {
         int n = bits.size();
 
-        int i=0;
-        while(i<n){
-            if(bits[i]==0){
+        if (n == 1 && bits[0] == 0)
+            return true;
+        if (n == 1 && bits[0] == 1)
+            return false;
+        int i = 0;
+        while (i < n) {
+            if (bits[i] == 0) {
                 i++;
-            }else{
-                i+=2;
+            } else {
+                i += 2;
             }
-            if(i==n-1){
+            if (i == n - 1) {
                 return true;
             }
         }
