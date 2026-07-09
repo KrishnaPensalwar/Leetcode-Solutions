cc09539a291b2fe01ec03b2e93a9cd0806aabf33
Krishna Pensalwar <100767867+KrishnaPensalwar@users.noreply.github.com>
2026-07-09 23:02:30 +0530

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 11.6 MB (49.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int a =0;
-        int b =0;
-        int c=0;
+        // int a =0;
+        // int b =0;
+        // int c=0;
 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                a++;
-            }else if(nums[i]==1){
-                b++;
-            }else{
-                c++;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0){
+        //         a++;
+        //     }else if(nums[i]==1){
+        //         b++;
+        //     }else{
+        //         c++;
+        //     }
+        // }
+
+        // for(int i=0;i<a;i++){
+        //     nums[i]=0;
+        // }
+
+        // for(int i=a;i<a+b;i++){
+        //     nums[i]=1;
+        // }
+        // for(int i=a+b;i<nums.size();i++){
+        //     nums[i]=2;
+        // }
+
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
             }
         }
-
-        for(int i=0;i<a;i++){
-            nums[i]=0;
-        }
-        
-        for(int i=a;i<a+b;i++){
-            nums[i]=1;
-        }
-        for(int i=a+b;i<nums.size();i++){
-            nums[i]=2;
-        }
-
     }
 };
